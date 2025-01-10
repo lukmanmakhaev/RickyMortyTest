8a7e9a4a3593905b6a8ca2fac2ef7364ae4449fa
Lukman <lukmanmakhaev@gmail.com>
2025-01-10 09:19:57 +0300

    Initial Commit

---
+++ RickyMortyTest/RickyMortyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortyTestApp.swift
+//  RickyMortyTest
+//
+//  Created by Lukman Makhaev on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
